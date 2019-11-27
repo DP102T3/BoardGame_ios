9db9fb3b15266e16b3966929804d0d908d8f98cd
hgjjgh <z7539518426q@gmail.com>
2019-11-27 13:54:55 +0800

    1.新增Login Group資料夾 2.新增LoginVC實作

---
+++ BoardGame_ios/Login/LoginVC.swift
@@ -0,0 +1,30 @@
+//
+//  LoginVC.swift
+//  BoardGame_ios
+//
+//  Created by 黃國展 on 2019/11/27.
+//  Copyright © 2019 黃國展. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
