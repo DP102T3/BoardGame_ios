75574bf1f95001a5c9fef6f4a7f298cabfd15b2e
hgjjgh <z7539518426q@gmail.com>
2019-11-27 10:39:50 +0800

    Initial Commit

---
+++ BoardGame_ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BoardGame_ios
+//
+//  Created by 黃國展 on 2019/11/27.
+//  Copyright © 2019 黃國展. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
